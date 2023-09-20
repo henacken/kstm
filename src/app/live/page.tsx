fc8408b56f19c6e7c1f1169b85f46ccc07c3ce7c
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-09-20 13:15:35 +0900

    サンプルをとりあえず貼り付け

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 //ライブ画面テンプレート
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import io from "socket.io-client";
 import ChatLogSpace, { ChatItem } from '@/components/organisms/ChatLogSpace'
 import MessageSend from '@/components/molecules/MessageSend'
 
-export default function archive() {
+export default function Live() {
   const chatLog: ChatItem[] = [
     { type: 'user', name: 'ogaog', comment: 'コメントああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ' },
     { type: 'user', name: 'あああああああああああああああああああああああああああああああaa', comment: 'コメントあああeeああああああああああああああ' },
@@ -11,6 +13,31 @@ export default function archive() {
     { type: 'user', name: 'あああああああaa', comment: 'コメントあああeeあaaaあああああああああああああ' },
     { type: 'user', name: 'ああaa', comment: 'コメントあああeeああああああああああああああ' }
   ]
+
+  const [socket, _] = useState(() => io());
+  //const router = useRouter();
+  //const { roomId, username } = router.query;
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.emit("join", "hello");
+    socket.on("connect", () => {
+      console.log("socket connected");
+      socket.emit("join", "hello");
+    }),
+      socket.on("message", (data) => {
+
+      });
+  }, []);
+
+  const buttonClick = () => {
+    socket.emit("message", { message: "test!" });
+  };
+
+  const sendMessage = (content: string) => {
+    socket.emit("message", { message: content })
+  };
+
   return (
     <div>
       <h1>live</h1>
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,39 @@
+const { createServer } = require('http')
 const express = require('express')
 const next = require('next')
+const { Server, Socket } = require('socket.io')
 
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const port = parseInt(process.env.PORT || '3000', 10)
 const nextHandle = nextApp.getRequestHandler()
 
-const expressApp = express()
-
 nextApp.prepare().then(() => {
+  const expressApp = express()
+  const server = createServer(expressApp)
+  const io = new Server()
+  io.attach(server)
+
+  expressApp.get('/socket', async (_: any, res: any) => {
+    res.send('hello, world')
+  })
+
+  io.on('connection', (socket: typeof Socket) => {
+    socket.on('join', (roomId: any) => {
+      socket.join(roomId)
+      console.log('joined room!')
+    })
+    socket.on('message', (data: any) => {
+      io.to(data.roomId).emit('message', {
+        message: data.message,
+        username: data.username,
+        id: `${data.username}-${20}`,
+      })
+    })
+  })
+
   expressApp.all('*', (req: any, res: any) => {
     return nextHandle(req, res)
   })
-  expressApp.listen(port)
+  server.listen(port)
 })
